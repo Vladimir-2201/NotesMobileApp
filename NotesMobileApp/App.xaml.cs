6fb6dac0dbf731d77ceec10dc04ed7cfa9c75073
Vladimir <vladimirzaloga01@gmail.com>
2024-04-06 16:44:08 +1000

    Initial commit

---
+++ NotesMobileApp/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace NotesMobileApp
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
---
+++ NotesMobileApp/Platforms/MacCatalyst/AppDelegate.cs
@@ -0,0 +1,10 @@
+﻿using Foundation;
+
+namespace NotesMobileApp
+{
+    [Register("AppDelegate")]
+    public class AppDelegate : MauiUIApplicationDelegate
+    {
+        protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+    }
+}
